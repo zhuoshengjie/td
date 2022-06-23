0c1f62ff64cfcbf5bdf7fe9d2b3a85cdfbdd1aa7
zhuoshengjie <47313110+zhuoshengjie@users.noreply.github.com>
2022-06-23 16:49:59 +0800

    Initial commit

---
+++ src/pages/list/filter/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <common-table />
+  </div>
+</template>
+<script lang="ts">
+import CommonTable from '../components/CommonTable.vue';
+
+export default {
+  name: 'ListFilter',
+  components: {
+    CommonTable,
+  },
+};
+</script>
